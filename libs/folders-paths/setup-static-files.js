8791647c1f8735da8a7b4328fd4b450aef578914
MANIKANDAN5747 <manikandan.p@germanexperts.ae>
2026-01-23 12:10:47 +0400

    ff

--- libs/folders-paths/setup-static-files.js
+++ libs/folders-paths/setup-static-files.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const path = require("path");
 const staticPaths = require("@libs/folders-paths/static-paths");
-const checkApiKey = require('@libs/JWT/apikey-auth-api');
+// const checkApiKey = require('@libs/JWT/apikey-auth-api');
+
 function setupStaticFiles(app) {
   staticPaths.forEach(({ route, dir }) => {
     const resolvedPath = path.resolve(dir);
     // Apply API Key middleware before serving static files
-    app.use(route, checkApiKey, express.static(resolvedPath));
+    app.use(route,  express.static(resolvedPath));
     console.log(`📁 Serving static path: ${route} → ${resolvedPath}`);
   });
 }

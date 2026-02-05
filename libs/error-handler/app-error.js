f413249c4d77cd32c0b3890fd86b64bffbcc2db8
MANIKANDAN5747 <manikandan.p@germanexperts.ae>
2026-02-05 14:39:01 +0400

    error

--- libs/error-handler/app-error.js
+++ libs/error-handler/app-error.js
@@ -2,12 +2,14 @@ class AppError extends Error {
   constructor(msg, statusCode, fields) {
     super(msg);
     this.header_type = "ERROR",
-      this.message_visibility = true,
-      this.statusCode = statusCode;
-      this.status = false;
-      this.code = 2004,
-      this.fields = fields;
-     
+    this.message_visibility = true,
+    this.status = false;
+    this.message = msg,
+    this.error = msg,
+    this.statusCode = statusCode;
+    this.code = 2004,
+    this.fields = fields;
+
 
     // Capture stack trace
     Error.captureStackTrace(this, this.constructor);

72fb854d525b0c3ea3cc199556bb6910ca3e9f70
MANIKANDAN5747 <manikandan.p@germanexperts.ae>
2026-03-02 15:51:56 +0400

    check

--- libs/requesters/admin/iam-requesters/users-requester.js
+++ libs/requesters/admin/iam-requesters/users-requester.js
@@ -3,8 +3,8 @@ const cote = require('cote');
 const redisHost = process.env.COTE_DISCOVERY_REDIS_HOST || '127.0.0.1';
 
 const usersRequester = new cote.Requester({
-  name: 'admin-users requester',
-  key: 'admin-users',
+  name: 'admin-iam-users requester',
+  key: 'admin-iam-users',
   redis: {
     host: redisHost,
     port: 6379

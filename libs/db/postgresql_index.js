abd8768c0412087b52e01bcad2575c69f9e44ecf
MANIKANDAN5747 <manikandan.p@germanexperts.ae>
2026-03-02 15:03:39 +0400

    PostgreSQL connected

--- libs/db/postgresql_index.js
+++ libs/db/postgresql_index.js
@@ -2,11 +2,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "host.docker.internal",
+  host: process.env.DB_HOST || "10.33.30.5",
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "admin",
-  database: process.env.DB_NAME || "eparts_development",
+  password: process.env.DB_PASSWORD || "ExZ3naPw@32!perts",
+  database: process.env.DB_NAME || "eparts_core",
 });
 
 pool.on("connect", () => {

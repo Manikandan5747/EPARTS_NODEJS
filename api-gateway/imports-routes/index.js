5b4e37c22d29708d2772447122994a23ecb01eff
MANIKANDAN5747 <manikandan.p@germanexperts.ae>
2026-03-06 12:21:08 +0400

    docker file changes

--- api-gateway/imports-routes/index.js
+++ api-gateway/imports-routes/index.js
@@ -52,16 +52,16 @@ module.exports = function registerRoutes(app, middlewares) {
     const {  checkApiKey,assignAssignedTo } = middlewares;
 
     /* ---------------- ADMIN FLOW ---------------- */
-    app.use('/api', assignAssignedTo, AdminIAMRoutes);
-    app.use('/api', assignAssignedTo, AdminUserRoutes);
-    app.use('/api', assignAssignedTo, AdminProductRoutes);
-    app.use('/api', assignAssignedTo, AdminOrderRoutes);
-    app.use('/api', assignAssignedTo, AdminPaymentRoutes);
+    // app.use('/api', assignAssignedTo, AdminIAMRoutes);
+    // app.use('/api', assignAssignedTo, AdminUserRoutes);
+    // app.use('/api', assignAssignedTo, AdminProductRoutes);
+    // app.use('/api', assignAssignedTo, AdminOrderRoutes);
+    // app.use('/api', assignAssignedTo, AdminPaymentRoutes);
     // app.use('/api', assignAssignedTo, AdminInventoryRoutes);
     // app.use('/api', assignAssignedTo, AdminNotificationRoutes);
     // app.use('/api', assignAssignedTo, AdminSubscriptionRoutes);
     // app.use('/api', assignAssignedTo, AdminReportingRoutes);
-    app.use('/api', assignAssignedTo, AdminRequestOfferRoutes);
+    // app.use('/api', assignAssignedTo, AdminRequestOfferRoutes);
     app.use('/api', assignAssignedTo, AdminMasterRoutes);
     // app.use('/api', assignAssignedTo, AdminIntegrationRoutes);
     // app.use('/api', assignAssignedTo, AdminCMSRoutes);
